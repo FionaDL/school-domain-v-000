0a983b7ebde447c222160212af372cfafd011c45
Fiona Lapham <fionadlapham@gmail.com>
2018-02-08 15:24:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
+require 'pry'# code herve!
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+ def add_student (student, grade)
+   if @roster[grade] == nil
+   @roster[grade] = []
+   @roster[grade] << student
+  else
+    @roster[grade] = [student]
+  end
+ end
+end
